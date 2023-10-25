f1dc4cf2b004e04dd419c478a95f217fff180972
kaserei <kaserei@gmail.com>
2023-10-25 10:52:01 +0100

    Added metadata and basic code

---
+++ Time Simple/src/time_simple@4t0m1c4/desklet.js
@@ -0,0 +1,24 @@
+const Deskklet = imports.ui.desklet;
+const St = imports.gi.St;
+
+function MyDesklet(metadata, desklet_id) {
+    this._init(metadata, desklet_id);
+}
+
+MyDesklet.prototype = {
+
+    __proto__: Desklet.Desklet.prototype,
+
+    _init: function (metadata, desklet_id) {
+        Desklet.Desklet.prototype._init.call(this.metadata, desklet_id);
+        this.setupUI();
+    },
+
+    setupUI() {
+        this.window = new St.Bin();
+        this.text = new St.Label({ text: "Hello Desktop" });
+        this.window.add_actor(this.text);
+    },
+
+}
+
