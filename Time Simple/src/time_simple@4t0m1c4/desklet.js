111fa799e54ad4787261e7bec3f3d3f6f6f223ed
kaserei <kaserei@gmail.com>
2023-10-29 19:54:52 +0000

    code copied from internet works

--- Time Simple/src/time_simple@4t0m1c4/desklet.js
+++ Time Simple/src/time_simple@4t0m1c4/desklet.js
@@ -5,29 +5,26 @@ function HelloDesklet(metadata, desklet_id) {
     this._init(metadata, desklet_id);
 }
 
-function main(metadata, desklet_id) {
-    return new HelloDesklet(metadata, desklet_id);
-}
-
-
 HelloDesklet.prototype = {
     __proto__: Desklet.Desklet.prototype,
 
-    _init: function (metadata, desklet_id) {
-
-        Desklet.Desklet.prototype._init.call(this, metadata);
+    _init: function(metadata, desklet_id) {
+        Desklet.Desklet.prototype._init.call(this, metadata, desklet_id);
 
         this.setupUI();
-
     },
 
-    setupUI: function () {
+    setupUI: function() {
         // main container for the desklet
         this.window = new St.Bin();
         this.text = new St.Label();
         this.text.set_text("Hello, world!");
-
+        
         this.window.add_actor(this.text);
         this.setContent(this.window);
-    },
+    }
+}
+
+function main(metadata, desklet_id) {
+    return new HelloDesklet(metadata, desklet_id);
 }
